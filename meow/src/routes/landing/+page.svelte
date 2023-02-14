dc5990c5791e9f3eff73087454543dd8e33e8723
Avery Cunningham <ave210knife@gmail.com>
2023-02-14 06:49:43 +0000

    Configure MQTT and save images to mongo

--- meow/src/routes/landing/+page.svelte
+++ meow/src/routes/landing/+page.svelte
@@ -1,8 +1,55 @@
+<script>
+	export let data;
+	console.log(data);
+	
+	import { time } from '../../store.js';
+
+	const formatter = new Intl.DateTimeFormat('en', {
+		hour12: true,
+		hour: 'numeric',
+		minute: '2-digit',
+		second: '2-digit'
+	});
+</script>
+
 <svelte:head>
-	<title>Landing Page</title>
-	<meta name="description" content="Landing Page" />
+	<title>Home</title>
+	<meta name="description" content="CATS demo app" />
 </svelte:head>
 
-<div>
-    <p>This will be the landing page for a returning user</p>
-</div>
+<section class= "flex flex-col justify-items-center align-middle">
+	<h1 class="mt-5">C.A.T.S</h1>
+	<h1 class="mb-5">Camera Assisted Tracking System</h1>
+	<span class="flex justify-center">
+		<div class="box">
+			<h2>Images load in here from API</h2>
+		</div>
+
+		<div class="box">
+			<h1>The time is {formatter.format($time)}</h1>
+		</div>	
+	</span>
+	<span class="flex justify-center">
+		<div class="box">
+			<h2>Words</h2>
+		</div>
+
+		<div class="box">
+			<h1>Words</h1>
+		</div>	
+	</span>
+
+</section>
+
+<style>
+	.box {
+		width: 300px;
+		background-color: white;
+		border: 1px solid rgb(170, 170, 170);
+		border-radius: 20px;
+		box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+		padding: 3em;
+		margin: 4px 4px 4px 4px;
+	}
+
+</style>
---
+++ purr/mqtt_to_mongodb.py
@@ -0,0 +1,45 @@
+import paho.mqtt.client as mqtt
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import datetime
+import os
+
+#MongoDB code
+def get_database():
+ 
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   CONNECTION_STRING = f"mongodb://{os.environ['MONGODB_USER']}:{os.environ['MONGODB_PASSWORD']}@mongodb" 
+#       CONNECTION_STRING = f"mongodb://{os.environ['MONGODB_USER']}:{os.environ['MONGODB_PASSWORD']}@localhost/test?retryWrites=true&w=majority" 
+
+   print(CONNECTION_STRING)
+ 
+#    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+ 
+#    # Create the database for our example (we will use the same database throughout the tutorial
+   return client[os.environ['MONGODB_DATABASE']]
+  
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("frigate/+/+/snapshot")
+def on_message(client, userdata, msg):
+    print(msg.topic + " @ " + str(datetime.datetime.now()) + str(msg.payload))
+    # print(msg.properties)
+
+    camera = msg.topic.split("/");
+    newID = ObjectId()
+    filepath = f"/images/{camera[1]}-{newID}.jpeg"
+
+    with open(filepath, 'wb') as file_handler:
+        file_handler.write(msg.payload) 
+
+    collection.insert_one({'_id': newID, 'path': filepath, 'object': camera[2]})
+
+db = get_database()
+collection = db['cat_images']
+print("python running!")
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("mosquitto", 1883, 60)
+client.loop_forever()
