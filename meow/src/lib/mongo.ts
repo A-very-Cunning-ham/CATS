2f8987273bcdbc309d1748aa0264f6186923c7a5
Adrian <adgarcia1119@gmail.com>
2023-03-30 12:40:59 -0400

    minor visual updates

--- meow/src/lib/mongo.ts
+++ meow/src/lib/mongo.ts
@@ -2,7 +2,7 @@ import {MongoClient} from 'mongodb';
 
 // TODO: generate these values using .env
 
-const client = new MongoClient('mongodb+srv://william:123@cluster0.2nxfqjy.mongodb.net/?retryWrites=true&w=majority')
+const client = new MongoClient('mongodb+srv://william:12345@cluster0.2nxfqjy.mongodb.net/?retryWrites=true&w=majority')
 
 export function start_mongo() {
 	console.log('Starting mongo...');
