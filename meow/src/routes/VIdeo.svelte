e3b701b8902a0cdecb8c3a1333a20e2811679028
Adrian <adgarcia1119@gmail.com>
2023-04-10 21:34:26 -0400

    upload lib files

--- meow/src/routes/VIdeo.svelte
+++ meow/src/routes/VIdeo.svelte
@@ -2,4 +2,4 @@
     import { Video } from 'flowbite-svelte';
 </script>
 
-<Video src='../lib/images/video/WGI_0010.AVI' controls trackSrc='WGI_0010.AVI' />
+<Video src='https://github.com/A-very-Cunning-ham/CATS/blob/ui-fixes/meow/src/lib/images/video/WGI_0010.AVI' controls trackSrc='WGI_0010.AVI' />
